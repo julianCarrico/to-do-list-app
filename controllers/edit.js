77823982aca4721a43fb41d3963f7a2eb3cb9690
Julian Carrico <Julian.carrico@gmail.com>
2026-02-20 01:34:20 -0500

    fixed an issue with edit that showed list content from other users

--- controllers/edit.js
+++ controllers/edit.js
@@ -3,8 +3,8 @@ const TodoTask = require('../models/Todotask')
 module.exports = {
     getEdit: (req, res) => {
         const id = req.params.id;
-        TodoTask.find({}, (err, tasks) => {
-            res.render("edit.ejs", { todoTasks: tasks, idTask: id });
+        TodoTask.find({ userId: req.user.id }, (err, tasks) => {
+            res.render("edit.ejs", { todoTasks: tasks, idTask: id, user: req.user });
         });
     },
     deleteTask: (req, res) => {
@@ -20,7 +20,8 @@ module.exports = {
             id,
             {
                 title: req.body.title,
-                content: req.body.content
+                content: req.body.content,
+                userId: req.user.id
             },
             err => {
                 if (err) return res.status(500).send(err);
