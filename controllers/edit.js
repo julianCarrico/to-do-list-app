ad12064d42c95bbc01a20cfaed5c2c0f100eed22
Julian Carrico <Julian.carrico@gmail.com>
2026-03-04 13:27:52 -0500

    updated some packages that were outdated, updated edit.js to async await to fix no callback error for mongoose 9

--- controllers/edit.js
+++ controllers/edit.js
@@ -1,31 +1,50 @@
 const TodoTask = require('../models/Todotask')
 
 module.exports = {
-    getEdit: (req, res) => {
-        const id = req.params.id;
-        TodoTask.find({ userId: req.user.id }, (err, tasks) => {
-            res.render("edit.ejs", { todoTasks: tasks, idTask: id, user: req.user });
-        });
-    },
-    deleteTask: (req, res) => {
-        const id = req.params.id;
-        TodoTask.findByIdAndRemove(id, err => {
-            if (err) return res.send(500, err);
-            res.redirect("/todos");
-        });
-    },
-    updateTask: (req, res) => {
-        const id = req.params.id;
-        TodoTask.findByIdAndUpdate(
-            id,
-            {
-                title: req.body.title,
-                content: req.body.content,
-                userId: req.user.id
-            },
-            err => {
-                if (err) return res.status(500).send(err);
-                res.redirect("/todos");
+    getEdit: async (req, res) => {
+        try {
+            const id = req.params.id;
+            // Mongoose 9: await the find call instead of using a callback
+            const tasks = await TodoTask.find({ userId: req.user.id });
+
+            res.render("edit.ejs", {
+                todoTasks: tasks,
+                idTask: id,
+                user: req.user
             });
+        } catch (err) {
+            console.error("Error fetching tasks for edit:", err);
+            res.redirect("/todos"); // or your preferred error route
+        }
+    },
+    deleteTask: async (req, res) => {
+        try {
+            const id = req.params.id;
+            // Mongoose 9: findByIdAndRemove is replaced by findByIdAndDelete
+            await TodoTask.findByIdAndDelete(id);
+            res.redirect("/todos");
+        } catch (err) {
+            console.error("Delete Error:", err);
+            res.status(500).send(err);
+        }
+    },
+
+    updateTask: async (req, res) => {
+        try {
+            const id = req.params.id;
+            // Mongoose 9: await the update call (no callbacks)
+            await TodoTask.findByIdAndUpdate(
+                id,
+                {
+                    title: req.body.title,
+                    content: req.body.content,
+                    userId: req.user.id
+                }
+            );
+            res.redirect("/todos");
+        } catch (err) {
+            console.error("Update Error:", err);
+            res.status(500).send(err);
+        }
     }
 }
